41e1334d75d50d375203744ed56b0a8234031aa9
Bill Scriven <bmscriven@gmail.com>
2019-12-05 09:51:07 +1000

    updating index with localhost ammps config update;

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>TITLE</title>
+	<title>Bms Git Connect Test</title>
 </head>
 <body>
 	
 	<table border='1'>
 		<tr><td colspan='2'>Table Heading</td><tr>
 		<tr><th>ID</td><td>Name</td>
-	</table>
-
+		<tr><th>Dirty Bird </td><td>Name</td>
 <?php
-$count=0
-$name="";
-while ($count<10) {
-	$count++;
-	$id="id$count";
-	$name.="a";
-	echo "<tr>
-		<td>$id</td>
-		<td>$name</td>
-		";
+for ($i=0; $i < 10; $i++) { 
+	$html="<tr><td colspan='2'>Echo $i</td><tr>";
+	echo $html;
 }
-
-
 ?>
+
+
+
+	</table>
 		
 
 </body>
